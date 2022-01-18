de86113992d23390842772c981c6e06e4dfd3980
darkphoenix17 <karanmistry284@gmail.com>
2022-01-18 22:45:49 +0530

    SignUp added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+    include SessionsHelper
+
+    def new
+    end
+
+    def create
+        @user = User.create(params.require(:user).permit(:name, :email, :password))
+        log_in(@user)
+        redirect_to(root_path)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root   'home#index'
   get    'edit'    => 'home#edit'
+  get    'signup'  => 'users#new'
+  post   'signup'  => 'users#create'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
 
+  # resources :users, only: [:new]
   resources :profiles , only: [:update]
   resources :educations , only: [:new]
 end
